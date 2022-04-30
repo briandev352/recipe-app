2e1a003cb124af0c5df520c6f6a6f4e956eae9a1
briandev352 <brianjenney930@gmail.com>
2022-04-30 17:16:01 -0400

    implement tab selection behavior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { TabOptions } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'recipe-app';
+  tab: TabOptions = 'Recipes';
+
+  handleTabClick(tab: TabOptions) {
+    this.tab = tab;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+export type TabOptions = 'Recipes' | 'Shopping List';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  @Output() tabClick = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  handleTabSelection(tab: TabOptions) {
+    this.tabClick.emit(tab);
   }
-
 }
